f0ebfe67a46f3a46326990bc1738fbda9866c624
deexcapp <deexcapp@gmail.com>
2024-11-13 20:47:56 +0600

    Update RefRewards.js
    Signed-off-by: deexcapp <deexcapp@gmail.com>

--- src/Components/RefRewards.js
+++ src/Components/RefRewards.js
@@ -42,7 +42,7 @@ const RefRewards = () => {
               <p className="font-medium text-center">Great job!</p>
             </div>
             <h3 className="font-medium text-[20px] text-[#ffffff] pt-2 pb-2">
-              <span className="text-accent">+{formatNumber(reward.bonusAward)}</span> $GHOST CLAIMED
+              <span className="text-accent">+{formatNumber(reward.bonusAward)}</span> $ADS CLAIMED
             </h3>
             <p className="pb-6 text-[#bfbfbf] text-[15px] w-full text-center">
               Keep inviting friends to unlock new rewards! Amazing things await!
@@ -116,7 +116,7 @@ const RefRewards = () => {
                     <span className='flex text-secondary items-center w-fit text-[15px]'>
                    
                       <span className=''>
-                        +{formatNumber(reward.bonusAward)} $GHOST
+                        +{formatNumber(reward.bonusAward)} $ADS
                       </span>
                     </span>
                   </div>
